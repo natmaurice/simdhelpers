2b89282c91f73678f8c45f7a4172fd654e522df2
Nathan Maurice <nathan.maurice@lip6.fr>
2022-11-04 19:03:41 +0100

    Use posix_memalign when aligned_alloc not available

--- include/simdhelpers/aligned_alloc.hpp
+++ include/simdhelpers/aligned_alloc.hpp
@@ -17,7 +17,16 @@ void aligned_delete(T* ptr, size_t alignment);
 // Implementations
 template <typename T>
 T* aligned_new(size_t size, size_t alignment) {
-    return (T*)aligned_alloc(alignment, roundup_kpow2(size * sizeof(T), alignment));
+#ifdef _ISOC11_SOURCE
+  return (T*)aligned_alloc(alignment, roundup_kpow2(size * sizeof(T), alignment));
+#elif _POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600
+  void* ptr;
+  posix_memalign(&ptr, alignment, roundup_kpow2(size * sizeof(T), alignment));
+  return reinterpret_cast<T*>(ptr);
+
+#else
+  return NULL;
+#endif // 
 }
 
 template <typename T>
