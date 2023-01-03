80d276fc15936dce154e6bb2dfc55cf3ba955348
Nathan Maurice <nathan.maurice@protonmail.com>
2023-01-03 16:17:42 +0100

    Added expand_select_2x32x4

--- include/simdhelpers/expand/expand-sse.hpp
+++ include/simdhelpers/expand/expand-sse.hpp
@@ -23,6 +23,22 @@ inline int expand_32x4(__m128i a, __m128i mask, __m128i& res) {
     return count;
 }
 
+inline int expand_select_2x32x4(__m128i a, __m128i b, __m128i mask, __m128i& res) {
+    __m128i shufm0, shufm1;
+    int m0 = ::sse::movemask_32x4(mask);
+    int m1 = ~m0;
+    
+    shufm0 = _mm_load_si128((__m128i*)(expand_LUT32x4[m0]));
+    shufm1 = _mm_load_si128((__m128i*)(expand_LUT32x4[m1]));
+
+    __m128i r0 = _mm_shuffle_epi8(a, shufm0);
+    __m128i r1 = _mm_shuffle_epi8(b, shufm1);
+
+    res = _mm_blendv_epi8(r1, r0, mask);
+    
+    return __builtin_popcount(m0);
+}
+
 }
 }
 
